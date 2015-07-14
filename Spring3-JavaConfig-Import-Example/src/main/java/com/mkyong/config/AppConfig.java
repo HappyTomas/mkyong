99094c770a454c2337ee123f36b7a930540b4df3
shareef hiasat <shareef.hiasat@gmail.com>
2015-07-14 23:41:04 +0300

    http://www.mkyong.com/spring/spring-di-via-setter-method/

--- Spring3-JavaConfig-Import-Example/src/main/java/com/mkyong/config/AppConfig.java
+++ Spring3-JavaConfig-Import-Example/src/main/java/com/mkyong/config/AppConfig.java
@@ -8,3 +8,19 @@ import org.springframework.context.annotation.Import;
 public class AppConfig {
 
 }
+
+
+/* xml config style is like this
+
+	<beans xmlns="http://www.springframework.org/schema/beans"
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://www.springframework.org/schema/beans
+		http://www.springframework.org/schema/beans/spring-beans-2.5.xsd">
+	 
+		<import resource="common/Spring-Common.xml"/>
+	        <import resource="connection/Spring-Connection.xml"/>
+	        <import resource="moduleA/Spring-ModuleA.xml"/>
+	 
+	</beans>
+
+*/
